64f5dd4db08baa8ab5d2e0a3f7ff91f88d446bf4
zeyad <test>
2026-08-31 22:51:50 +0300

    Ecom-Signin

--- src/test/java/EcommerceTest1.java
+++ src/test/java/EcommerceTest1.java
@@ -1,5 +1,6 @@
 import io.appium.java_client.AppiumBy;
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -10,7 +11,15 @@ public class EcommerceTest1 extends BaseTest{
     public void userCanSignInApp(){
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
         driver.findElement(By.id("com.androidsample.generalstore:id/spinnerCountry")).click();
-        driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().text(\"Egypt\")"));
+        driver.findElement(AppiumBy.androidUIAutomator(
+                "new UiScrollable(new UiSelector().scrollable(true).instance(0))"
+                        + ".scrollIntoView(new UiSelector().text(\"Egypt\"))"
+        ));
         driver.findElement(By.xpath("//android.widget.TextView[@resource-id=\"android:id/text1\" and @text=\"Egypt\"]\n")).click();
+//        driver.findElement(By.xpath("//android.widget.EditText[@resource-id=\"com.androidsample.generalstore:id/nameField\"]")).sendKeys("Zezo");
+        driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().resourceId(\"com.androidsample.generalstore:id/btnLetsShop\")")).click();
+        String massage= driver.findElement(By.xpath("(//android.widget.Toast)[1]")).getText();
+        Assert.assertEquals(massage,"Please enter your name");
+
     }
 }
