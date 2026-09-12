ca299cc8e714350979eae0895b1a3276298741bf
zeyad <test>
2026-09-13 00:35:58 +0300

    Assert total amount

--- src/test/java/EcommerceTest3.java
+++ src/test/java/EcommerceTest3.java
@@ -1,12 +1,15 @@
 import io.appium.java_client.AppiumBy;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
 import java.time.Duration;
+import java.util.List;
 import java.util.function.Function;
 
 public class EcommerceTest3 extends BaseTest{
@@ -26,6 +29,23 @@ public class EcommerceTest3 extends BaseTest{
         driver.findElements(By.id("com.androidsample.generalstore:id/productAddCart")).get(0).click();
         driver.findElements(By.id("com.androidsample.generalstore:id/productAddCart")).get(1).click();
         driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+        int cardProduct=driver.findElements(By.id("com.androidsample.generalstore:id/productPrice")).size();
+
+        double totalPrice=0;
+         for(int i=0; i<cardProduct; i++){
+             String prePaying =driver.findElements(By.id("com.androidsample.generalstore:id/productPrice")).get(i).getText();
+
+             double price = Double.parseDouble(prePaying.substring(1));
+             totalPrice= price+totalPrice;
+
+        }
+         String CalculatedAmount= driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().resourceId(\"com.androidsample.generalstore:id/totalAmountLbl\")")).getText();
+         double calculatedPrice= Double.parseDouble(CalculatedAmount.substring(1));
+         Assert.assertEquals(calculatedPrice,totalPrice);
+         
+
 
 
     }
